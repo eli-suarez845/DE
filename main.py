16ee2e8d239d45d33e00f1386b3b931b171672d1
eli-suarez845 <elisasuarezmoreira@gmail.com>
2023-11-05 13:43:40 -0300

    Reentrega Entregable 1

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-# Instalamos librerías:
-# Packages instalados: sqlalchemy-redshift, pandas
 
-# Importamos librerías:
+# Python Packages instalados: sqlalchemy-redshift, psycopg2
+
+# Se importa librerías:
 from configparser import ConfigParser
-import pandas as pd
 import sqlalchemy as sa
 
-# Definimos funciones:
+# Se define funciones:
 
 def build_conn_string(config_path, config_section):
     """
@@ -14,11 +13,11 @@ def build_conn_string(config_path, config_section):
     a partir de un archivo de configuración.
     """
 
-    # Lee el archivo de configuración
+    # Para leer el archivo de configuración
     parser = ConfigParser()
     parser.read(config_path)
 
-    # Lee la sección de configuración de PostgreSQL
+    # Parámetros de conexión a Redshift
     config = parser[config_section]
     host = config['host']
     port = config['port']
@@ -26,7 +25,7 @@ def build_conn_string(config_path, config_section):
     username = config['username']
     pwd = config['pwd']
 
-    # Construye la cadena de conexión
+    # Se construye la cadena de conexión
     conn_string = f'postgresql://{username}:{pwd}@{host}:{port}/{dbname}?sslmode=require'
 
     return conn_string
@@ -42,8 +41,39 @@ def connect_to_db(conn_string):
     return conn, engine
 
 
-# Conexión a Redshift
+# ----------------------------
+
+# Se establece la conexión a Redshift
 config_dir = "venv/config/config.ini"
 conn_string = build_conn_string(config_dir, "redshift")
 conn, engine = connect_to_db(conn_string)
+
+schema = "elisasuaezmoreira_coderhouse"
+
+
+# Se define script una tabla
+create_table_query = """
+CREATE TABLE IF NOT EXISTS {schema}.PrimerEntregable2(
+    article_id VARCHAR(50) PRIMARY KEY distkey,
+    title VARCHAR(100),
+    creator VARCHAR(50),
+    link VARCHAR(100),
+    pubDate DATE,
+    language VARCHAR(50),
+    category VARCHAR(50),
+    country VARCHAR(50),
+    description VARCHAR(500)
+)
+SORTKEY (pubDate);
+"""
+
+
+# Se ejecuta
+conn.execute(create_table_query)
+
+
+
+
+
+
 
